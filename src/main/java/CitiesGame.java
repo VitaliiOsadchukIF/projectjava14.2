99cb197cf5c030819eb488b334ea958426a921d0
radcheenkoo <134805103+radcheenkoo@users.noreply.github.com>
2023-09-02 19:53:47 +0300

    fdgsdergrserg

--- src/main/java/CitiesGame.java
+++ src/main/java/CitiesGame.java
@@ -2,6 +2,6 @@ import javax.swing.*;
 
 public class CitiesGame {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new GameFrame());
+        GameFrame gameFrame = new GameFrame();
     }
 }
